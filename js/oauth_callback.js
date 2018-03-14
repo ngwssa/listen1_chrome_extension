20b34474e1e8ba6cfd69a85ba64d641f998c1745
ngwssa <ngwssa@163.com>
2018-03-14 17:37:43 +0800

    '添加自己写的示例'

--- js/oauth_callback.js
+++
@@ -1,9 +0,0 @@
-
-/**
- * Get and send oauth tokens from query string.
- */
-
-chrome.runtime.sendMessage({type: 'code', query: window.location.search.substr(1)}, function(response) {
-    // window.open('', '_self', '');
-    // window.close();
-});
